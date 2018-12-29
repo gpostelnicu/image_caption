3037357787d4aaa684d533ef93cd1f0079c19401
Gheorghe Postelnicu <gheorghe.postelnicu@gmail.com>
2018-12-29 12:47:48 +0100

    Added script to download Flickr8k dataset.

---
+++ bin/download_dataset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+THIS_DIR=$(dirname $(readlink -f "$0"))
+DATASET_ROOT_DIR="$THIS_DIR/../data/flickr8k"
+mkdir -p $DATASET_ROOT_DIR
+cd $DATASET_ROOT_DIR
+
+echo 'Downloading and extracting Flickr8k dataset...'
+mkdir -p dataset
+cd dataset
+wget -nc http://nlp.cs.illinois.edu/HockenmaierGroup/Framing_Image_Description/Flickr8k_text.zip
+wget -nc http://nlp.cs.illinois.edu/HockenmaierGroup/Framing_Image_Description/Flickr8k_Dataset.zip
+unzip -n Flickr8k_text.zip -x '__MACOSX/*' -d Flickr8k_text > /dev/null
+unzip -nj Flickr8k_Dataset.zip -x '__MACOSX/*' -d Flickr8k_Dataset > /dev/null
+echo 'Done!'
