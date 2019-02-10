36b6ae3fcdf43eb4ff4bfc1b16280b000cfeecab
Gheorghe Postelnicu <gheorghe.postelnicu@gmail.com>
2019-02-10 11:36:03 +0000

    Fixed off-by-one bug with network output size.

--- image_caption/cli/base_trainer.py
+++ image_caption/cli/base_trainer.py
@@ -261,7 +261,7 @@ class ImageFirstE2ETrainer(E2ETrainer):
             img_embedding_shape=(224, 224, 3),
             text_embedding_matrix=embedding_matrix,
             max_caption_len=train_seq.max_length,
-            vocab_size=1 + len(tok.index_word),
+            vocab_size=len(tok.index_word),
             embedding_dim=embedding_dim + len(special_tokens),
             text_embedding_trainable=self.text_embedding_trainable,
             img_dense_dim=self.img_dense_dim,
